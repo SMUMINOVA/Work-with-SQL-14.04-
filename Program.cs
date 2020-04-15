01407526474a14c623560861f10988e03aa173bd
Sobira <sobira204@gmail.com>
2020-04-15 23:25:54 +0500

    using class

--- Program.cs
+++ Program.cs
@@ -7,13 +7,103 @@ namespace HW_15_04
     {
         static void Main(string[] args)
         {
-            const string conString = @"Data source=localhost; Initial catalog = Test; user id = sa;password=S1806Kh2111";
+            /*const string conString = @"Data source=localhost; Initial catalog = Test; user id = sa;password=S1806Kh2111";
             SqlConnection scon = new SqlConnection(conString);
             scon.Open();
             if(scon.State == ConnectionState.Open)
                 System.Console.WriteLine("Connected successfully");
             else 
-                System.Console.WriteLine("Oooops, some problems");
+                System.Console.WriteLine("Oooops, some problems");*/
+            System.Console.WriteLine("Welcom to FriendBook! Here you can note information about your friends");
+            System.Console.WriteLine("How many friends do you have?");
+            int n = int.Parse(Console.ReadLine());
+            SCommands firstEnter = new SCommands();
+            for (int i = 0; i < n; i++){
+                System.Console.Write($"Please enter Firstname of your {++i} friend: ");
+                firstEnter.FirstName = Console.ReadLine();
+                System.Console.Write($"Please enter Middlename of your {++i} friend: ");
+                firstEnter.MiddleName = Console.ReadLine();
+                System.Console.Write($"Please enter Lastname of your {++i} friend: ");
+                firstEnter.LastName = Console.ReadLine();
+                System.Console.Write($"Please enter date of birth of your {++i} friend(yyyy-mm-dd hh:mm:ss): ");
+                firstEnter.BirthDate = Console.ReadLine();
+                firstEnter.insertSCommand();
+            }
+            System.Console.WriteLine("Now you can:\n1. Add new friend\n2. Correct some information about your friends\n3. Delete your friends\n4. Get information about all of your friends\n5. Get information about your friends having specific information");
+            int choice = int.Parse(Console.ReadLine());
+            switch(choice){
+                case 1: {
+                    System.Console.Write($"Please enter Firstname of your friend: ");
+                    firstEnter.FirstName = Console.ReadLine();
+                    System.Console.Write($"Please enter Middlename of your friend: ");
+                    firstEnter.MiddleName = Console.ReadLine();
+                    System.Console.Write($"Please enter Lastname of your friend: ");
+                    firstEnter.LastName = Console.ReadLine();
+                    System.Console.Write($"Please enter date of birth of your friend(yyyy-mm-dd hh:mm:ss): ");
+                    firstEnter.BirthDate = Console.ReadLine();
+                    firstEnter.insertSCommand();
+                }; break;
+                case 2: {
+                    start3 :
+                    System.Console.WriteLine("What information do you want to correct?\n1. Firstname\n2. Middlename\n3. Lastname\n4. date of birth");
+                    choice = int.Parse(Console.ReadLine());
+                    switch(choice){
+                        case 1: firstEnter.updateFirstName(); break;
+                        case 2: firstEnter.updateMiddleName(); break;
+                        case 3: firstEnter.updateLastName(); break;
+                        case 4: firstEnter.updateBirthDate(); break;
+                        default: System.Console.WriteLine("Error! Try again;"); goto start3;
+                    }
+                    start4 :
+                    System.Console.WriteLine("By what information do you want to correct?\n1. id\n2. Firstname\n3. Middlename\n4. Lastname\n5. date of birth");
+                    n = int.Parse(Console.ReadLine());
+                    switch(n){
+                        case 1: firstEnter.choiceKey = "id"; break;
+                        case 2: firstEnter.choiceKey = "FirstName"; break;
+                        case 3: firstEnter.choiceKey = "MiddleName"; break;
+                        case 4: firstEnter.choiceKey = "LastName"; break;
+                        case 5: firstEnter.choiceKey = "BirthDate"; break;
+                        default: Console.WriteLine("Error! Try again;"); goto start4;
+                    }
+                    System.Console.WriteLine($"Please set {firstEnter.choiceKey}");
+                    firstEnter.key = Console.ReadLine();
+                    
+                }; break;
+                case 3: {
+                    start :
+                    System.Console.WriteLine("By what information do you want to delete?\n1. id\n2. Firstname\n3. Middlename\n4. Lastname\n5. date of birth");
+                    n = int.Parse(Console.ReadLine());
+                    switch(n){
+                        case 1: firstEnter.choiceKey = "id"; break;
+                        case 2: firstEnter.choiceKey = "FirstName"; break;
+                        case 3: firstEnter.choiceKey = "MiddleName"; break;
+                        case 4: firstEnter.choiceKey = "LastName"; break;
+                        case 5: firstEnter.choiceKey = "BirthDate"; break;
+                        default: System.Console.WriteLine("Error! Try again;"); goto start;
+                    }
+                    System.Console.WriteLine($"Please set {firstEnter.choiceKey}");
+                    firstEnter.key = Console.ReadLine();
+                    firstEnter.Delete();
+                    }; break;
+                    case 4: firstEnter.selectAllSCommand();break;
+                    case 5:{
+                        start1 :
+                        System.Console.WriteLine("By what information do you want to get information?\n1. id\n2. Firstname\n3. Middlename\n4. Lastname\n5. date of birth");
+                        n = int.Parse(Console.ReadLine());
+                        switch(n){
+                            case 1: firstEnter.choiceKey = "id"; break;
+                            case 2: firstEnter.choiceKey = "FirstName"; break;
+                            case 3: firstEnter.choiceKey = "MiddleName"; break;
+                            case 4: firstEnter.choiceKey = "LastName"; break;
+                            case 5: firstEnter.choiceKey = "BirthDate"; break;
+                            default: System.Console.WriteLine("Error! Try again;"); goto start1;
+                        }
+                        System.Console.WriteLine($"Please set {firstEnter.choiceKey}");
+                        firstEnter.key = Console.ReadLine();
+                        firstEnter.selectByIdSCommand();
+                    }; break;
+                }
+            }
             
         }
         class SCommands{
@@ -125,6 +215,5 @@ namespace HW_15_04
                     System.Console.WriteLine("Delete command successfull!!!");
                 }
             }
-        }
     }
 }
