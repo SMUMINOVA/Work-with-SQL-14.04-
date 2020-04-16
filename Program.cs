b53d3911e1c59b041b89e31739fcb1e083ceb2a0
Sobira <sobira204@gmail.com>
2020-04-16 19:40:55 +0500

    fix some bugs and create new file for class

--- Program.cs
+++ Program.cs
@@ -1,19 +1,13 @@
 ﻿using System;
 using System.Data;
 using System.Data.SqlClient;
+using SQLCOmmands;
 namespace HW_15_04
 {
     class Program
     {
         static void Main(string[] args)
         {
-            /*const string conString = @"Data source=localhost; Initial catalog = Test; user id = sa;password=S1806Kh2111";
-            SqlConnection scon = new SqlConnection(conString);
-            scon.Open();
-            if(scon.State == ConnectionState.Open)
-                System.Console.WriteLine("Connected successfully");
-            else 
-                System.Console.WriteLine("Oooops, some problems");*/
             System.Console.WriteLine("Welcom to FriendBook! Here you can note information about your friends");
             System.Console.WriteLine("How many friends do you have?");
             int n = int.Parse(Console.ReadLine());
@@ -25,10 +19,11 @@ namespace HW_15_04
                 firstEnter.MiddleName = Console.ReadLine();
                 System.Console.Write($"Please enter Lastname of your friend: ");
                 firstEnter.LastName = Console.ReadLine();
-                System.Console.Write($"Please enter date of birth of your friend(yyyy-mm-dd hh:mm:ss): ");
-                firstEnter.BirthDate = Console.ReadLine();
+                System.Console.Write($"Please enter date of birth of your friend(yyyy-mm-dd) ");
+                firstEnter.BirthDate = DateTime.Parse(Console.ReadLine());
                 firstEnter.insertSCommand();
             }
+            starter :
             System.Console.WriteLine("Now you can:\n1. Add new friend\n2. Correct some information about your friends\n3. Delete your friends\n4. Get information about all of your friends\n5. Get information about your friends having specific information");
             int choice = int.Parse(Console.ReadLine());
             switch(choice){
@@ -40,7 +35,7 @@ namespace HW_15_04
                     System.Console.Write($"Please enter Lastname of your friend: ");
                     firstEnter.LastName = Console.ReadLine();
                     System.Console.Write($"Please enter date of birth of your friend(yyyy-mm-dd hh:mm:ss): ");
-                    firstEnter.BirthDate = Console.ReadLine();
+                    firstEnter.BirthDate = DateTime.Parse(Console.ReadLine());
                     firstEnter.insertSCommand();
                 }; break;
                 case 2: {
@@ -103,121 +98,11 @@ namespace HW_15_04
                         firstEnter.selectByIdSCommand();
                     }; break;
                 }
+            
+            Console.WriteLine("Do you want to continue?y/n");
+            if (Console.ReadLine() == "y") goto starter;
+            else System.Console.WriteLine("Bye");
             }
             
-        }
-        class SCommands{
-            public int Id{get; set;}
-            public string LastName{get; set;}
-            public string FirstName {get; set;}
-            public string MiddleName {get; set;}
-            public string BirthDate{get; set;}
-            public string key {get; set;}
-            public string choiceKey{get; set;}
-            
-            public void insertSCommand(){
-                const string conString = @"Data source=localhost; Initial catalog = Test; user id = sa;password=S1806Kh2111";
-                SqlConnection scon = new SqlConnection(conString);
-                scon.Open();
-                string insertSqlCommand = string.Format($"insert into Person([FirstName],[MiddleName],[LastName],[BirthDate]) Values('{FirstName}','{MiddleName}', '{LastName}', {BirthDate})");
-                SqlCommand command = new SqlCommand(insertSqlCommand, scon);
-                var result = command.ExecuteNonQuery();
-                if (result > 0)
-                {
-                    System.Console.WriteLine("Insert command successfull!!!");
-                }
-            }
-            
-            public void selectAllSCommand(){
-                const string conString = @"Data source=localhost; Initial catalog = Test; user id = sa;password=S1806Kh2111";
-                SqlConnection scon = new SqlConnection(conString);
-                scon.Open();
-                string commandText = "Select * from Person";
-                SqlCommand command = new SqlCommand(commandText, scon);
-                SqlDataReader reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    System.Console.WriteLine($"ID:{reader.GetValue("Id")}, FirstName:{reader.GetValue("FirstName")},MiddleName:{reader.GetValue("MiddleName")}, LastName:{reader.GetValue("LastName")}, Date of Birth: {reader.GetValue("BirthDate")}");
-                }
-                reader.Close();
-                }
-            public void selectByIdSCommand(){
-                const string conString = @"Data source=localhost; Initial catalog = Test; user id = sa;password=S1806Kh2111";
-                SqlConnection scon = new SqlConnection(conString);
-                scon.Open();
-                string commandText = $"Select * from Person where id = {Id}";
-                SqlCommand command = new SqlCommand(commandText, scon);
-                SqlDataReader reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    System.Console.WriteLine($"ID:{reader.GetValue("Id")}, FirstName:{reader.GetValue("FirstName")},MiddleName:{reader.GetValue("MiddleName")}, LastName:{reader.GetValue("LastName")}, Date of Birth: {reader.GetValue("BirthDate")}");
-                }
-                reader.Close();
-                }
-            
-            public void updateLastName(){
-                const string conString = @"Data source=localhost; Initial catalog = Test; user id = sa;password=S1806Kh2111";
-                SqlConnection scon = new SqlConnection(conString);
-                scon.Open();
-                string insertSqlCommand = string.Format($"update Person set LastName = {LastName} where {choiceKey} = {key}");
-                SqlCommand command = new SqlCommand(insertSqlCommand, scon);
-                var result = command.ExecuteNonQuery();
-                if (result > 0)
-                {
-                    System.Console.WriteLine("Update command successfull!!!");
-                }
-            }
-            public void updateMiddleName(){
-                const string conString = @"Data source=localhost; Initial catalog = Test; user id = sa;password=S1806Kh2111";
-                SqlConnection scon = new SqlConnection(conString);
-                scon.Open();
-                string insertSqlCommand = string.Format($"update Person set MiddleName = {MiddleName} where {choiceKey} = {key}");
-                SqlCommand command = new SqlCommand(insertSqlCommand, scon);
-                var result = command.ExecuteNonQuery();
-                if (result > 0)
-                {
-                    System.Console.WriteLine("Update command successfull!!!");
-                }
-            }
-            public void updateFirstName(){
-                const string conString = @"Data source=localhost; Initial catalog = Test; user id = sa;password=S1806Kh2111";
-                SqlConnection scon = new SqlConnection(conString);
-                scon.Open();
-                string insertSqlCommand = string.Format($"update Person set FirstleName = {FirstName} where {choiceKey} = {key}");
-                SqlCommand command = new SqlCommand(insertSqlCommand, scon);
-                var result = command.ExecuteNonQuery();
-                if (result > 0)
-                {
-                    System.Console.WriteLine("Update command successfull!!!");
-                }
-            }
-            public void updateBirthDate(){
-                const string conString = @"Data source=localhost; Initial catalog = Test; user id = sa;password=S1806Kh2111";
-                SqlConnection scon = new SqlConnection(conString);
-                scon.Open();
-                string insertSqlCommand = string.Format($"update Person set BirthDate = {BirthDate} where {choiceKey} = {key}");
-                SqlCommand command = new SqlCommand(insertSqlCommand, scon);
-                var result = command.ExecuteNonQuery();
-                if (result > 0)
-                {
-                    System.Console.WriteLine("Update command successfull!!!");
-                }
-            }
-            public void Delete(){
-                const string conString = @"Data source=localhost; Initial catalog = Test; user id = sa;password=S1806Kh2111";
-                SqlConnection scon = new SqlConnection(conString);
-                scon.Open();
-                string insertSqlCommand = string.Format($"delete Person where {choiceKey} = {key}");
-                SqlCommand command = new SqlCommand(insertSqlCommand, scon);
-                var result = command.ExecuteNonQuery();
-                if (result > 0)
-                {
-                    System.Console.WriteLine("Delete command successfull!!!");
-                }
-            }
-
-
-
-            // в строке var result = command.ExecuteNonQuery();  оператор var дает ошибку при компиляции
     }
 }
---
+++ SQLCommands.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+namespace SQLCOmmands
+{
+    class SCommands{
+            public int Id{get; set;}
+            public string LastName{get; set;}
+            public string FirstName {get; set;}
+            public string MiddleName {get; set;}
+            public DateTime BirthDate{get; set;}
+            public string key {get; set;}
+            public string choiceKey{get; set;}
+            
+            public void insertSCommand(){
+                const string conString = @"Data source=localhost; Initial catalog = Test; user id = sa;password=S1806Kh2111";
+                SqlConnection scon = new SqlConnection(conString);
+                scon.Open();
+                string insertSqlCommand = string.Format($"insert into Person([FirstName],[MiddleName],[LastName],[BirthDate]) Values('{FirstName}','{MiddleName}', '{LastName}', '{BirthDate}')");
+                SqlCommand command = new SqlCommand(insertSqlCommand, scon);
+                var result = command.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    System.Console.WriteLine("Insert command successfull!!!");
+                }
+            }
+            
+            public void selectAllSCommand(){
+                const string conString = @"Data source=localhost; Initial catalog = Test; user id = sa;password=S1806Kh2111";
+                SqlConnection scon = new SqlConnection(conString);
+                scon.Open();
+                string commandText = "Select * from Person";
+                SqlCommand command = new SqlCommand(commandText, scon);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    System.Console.WriteLine($"ID:{reader.GetValue("Id")}, FirstName:{reader.GetValue("FirstName")},MiddleName:{reader.GetValue("MiddleName")}, LastName:{reader.GetValue("LastName")}, Date of Birth: {reader.GetValue("BirthDate")}");
+                }
+                reader.Close();
+                }
+            public void selectByIdSCommand(){
+                const string conString = @"Data source=localhost; Initial catalog = Test; user id = sa;password=S1806Kh2111";
+                SqlConnection scon = new SqlConnection(conString);
+                scon.Open();
+                string commandText = $"Select * from Person where id = {Id}";
+                SqlCommand command = new SqlCommand(commandText, scon);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    System.Console.WriteLine($"ID:{reader.GetValue("Id")}, FirstName:{reader.GetValue("FirstName")},MiddleName:{reader.GetValue("MiddleName")}, LastName:{reader.GetValue("LastName")}, Date of Birth: {reader.GetValue("BirthDate")}");
+                }
+                reader.Close();
+                }
+            
+            public void updateLastName(){
+                const string conString = @"Data source=localhost; Initial catalog = Test; user id = sa;password=S1806Kh2111";
+                SqlConnection scon = new SqlConnection(conString);
+                scon.Open();
+                string insertSqlCommand = string.Format($"update Person set LastName = {LastName} where {choiceKey} = {key}");
+                SqlCommand command = new SqlCommand(insertSqlCommand, scon);
+                var result = command.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    System.Console.WriteLine("Update command successfull!!!");
+                }
+            }
+            public void updateMiddleName(){
+                const string conString = @"Data source=localhost; Initial catalog = Test; user id = sa;password=S1806Kh2111";
+                SqlConnection scon = new SqlConnection(conString);
+                scon.Open();
+                string insertSqlCommand = string.Format($"update Person set MiddleName = {MiddleName} where {choiceKey} = {key}");
+                SqlCommand command = new SqlCommand(insertSqlCommand, scon);
+                var result = command.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    System.Console.WriteLine("Update command successfull!!!");
+                }
+            }
+            public void updateFirstName(){
+                const string conString = @"Data source=localhost; Initial catalog = Test; user id = sa;password=S1806Kh2111";
+                SqlConnection scon = new SqlConnection(conString);
+                scon.Open();
+                string insertSqlCommand = string.Format($"update Person set FirstleName = {FirstName} where {choiceKey} = {key}");
+                SqlCommand command = new SqlCommand(insertSqlCommand, scon);
+                var result = command.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    System.Console.WriteLine("Update command successfull!!!");
+                }
+            }
+            public void updateBirthDate(){
+                const string conString = @"Data source=localhost; Initial catalog = Test; user id = sa;password=S1806Kh2111";
+                SqlConnection scon = new SqlConnection(conString);
+                scon.Open();
+                string insertSqlCommand = string.Format($"update Person set BirthDate = {BirthDate} where {choiceKey} = {key}");
+                SqlCommand command = new SqlCommand(insertSqlCommand, scon);
+                var result = command.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    System.Console.WriteLine("Update command successfull!!!");
+                }
+            }
+            public void Delete(){
+                const string conString = @"Data source=localhost; Initial catalog = Test; user id = sa;password=S1806Kh2111";
+                SqlConnection scon = new SqlConnection(conString);
+                scon.Open();
+                string insertSqlCommand = string.Format($"delete Person where {choiceKey} = {key}");
+                SqlCommand command = new SqlCommand(insertSqlCommand, scon);
+                var result = command.ExecuteNonQuery();
+                if (result > 0)
+                {
+                    System.Console.WriteLine("Delete command successfull!!!");
+                }
+            }
+    }
+}
